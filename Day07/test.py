827e5328b619bafe5bbf9a3cd23131596252f3c9
nipengbo1995 <673242024@qq.com>
2019-12-18 17:16:20 +0800

    m2019/12/18

--- Day07/test.py
+++ Day07/test.py
@@ -1,1 +1,11 @@
-f = [x for x in range(1, 10)]
+# 创建集合的字面量语法
+set1 = {1, 2, 3, 3, 3, 2}
+print(set1)
+print('Length =', len(set1))
+# 创建集合的构造器语法(面向对象部分会进行详细讲解)
+set2 = set(range(1, 10))
+set3 = set((1, 2, 3, 3, 2, 1))
+print(set2, set3)
+# 创建集合的推导式语法()
+set4 = {}
+
